690b0e80f9a6205db0e8fb41afca4be2a3edb17f
Larry <yuetao1998@gmail.com>
2020-05-06 00:00:24 -0400

    created the frontend css, index, and js

--- jupiter/src/main/java/external/GitHubClient.java
+++ jupiter/src/main/java/external/GitHubClient.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -12,13 +16,17 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import entity.Item;
+import entity.Item.ItemBuilder;
 
 public class GitHubClient {
 	private static final String URL_TEMPLATE = "https://jobs.github.com/positions.json?description=%s&lat=%s&long=%s";
 	private static final String DEFAULT_KEYWORD = "developer";
 
-	public JSONArray search(double lat, double lon, String keyword) {
+	public List<Item> search(double lat, double lon, String keyword) {
 		if (keyword == null) {
 			keyword = DEFAULT_KEYWORD;
 		}
@@ -32,11 +40,11 @@ public class GitHubClient {
 		try {
 			CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
 			if (response.getStatusLine().getStatusCode() != 200) {
-				return new JSONArray();
+				return new ArrayList<>();
 			}
 			HttpEntity entity = response.getEntity();
 			if (entity == null) {
-				return new JSONArray();
+				return new ArrayList<>();
 			}
 			BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
 			StringBuilder responseBody = new StringBuilder();
@@ -44,12 +52,54 @@ public class GitHubClient {
 			while ((line = reader.readLine()) != null) {
 				responseBody.append(line);
 			}
-			return new JSONArray(responseBody.toString());
+			JSONArray array = new JSONArray(responseBody.toString());
+			return getItemList(array);
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return new JSONArray();
+		return new ArrayList<>();
+	}
+	private List<Item> getItemList(JSONArray array) {
+		List<Item> itemList = new ArrayList<>();
+		List<String> descriptionList = new ArrayList<>();
+		for (int i = 0; i < array.length(); i++) {
+			// We need to extract keywords from description since GitHub API
+			// doesn't return keywords.
+			String description = getStringFieldOrEmpty(array.getJSONObject(i), "description");
+			if (description.equals("") || description.equals("\n")) {
+				descriptionList.add(getStringFieldOrEmpty(array.getJSONObject(i), "title"));
+			} else {
+				descriptionList.add(description);
+			}	
+		}
+
+		// We need to get keywords from multiple text in one request since
+		// MonkeyLearnAPI has limitations on request per minute.
+		List<List<String>> keywords = MonkeyLearnClient
+				.extractKeywords(descriptionList.toArray(new String[descriptionList.size()]));
+
+
+		for (int i = 0; i < array.length(); ++i) {
+			JSONObject object = array.getJSONObject(i);
+			ItemBuilder builder = new ItemBuilder();
+			
+			builder.setItemId(getStringFieldOrEmpty(object, "id"));
+			builder.setName(getStringFieldOrEmpty(object, "title"));
+			builder.setAddress(getStringFieldOrEmpty(object, "location"));
+			builder.setUrl(getStringFieldOrEmpty(object, "url"));
+			builder.setImageUrl(getStringFieldOrEmpty(object, "company_logo"));
+			List<String> curKeywords = keywords.get(i);
+			builder.setKeywords(new HashSet<String>(curKeywords));
+			Item item = builder.build();
+			itemList.add(item);
+		}
+		
+		return itemList;
+	}
+	
+	private String getStringFieldOrEmpty(JSONObject obj, String field) {
+		return obj.isNull(field) ? "" : obj.getString(field);
 	}
 }
